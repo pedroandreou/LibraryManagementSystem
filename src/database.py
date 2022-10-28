a639518fd0ef3ceaec1427624d44e1f89bfc622a
pedroandreou <pandreou01@gmail.com>
2022-10-28 15:26:04 +0100

    Update table schemas and normalize data

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 
 
 def create_connection(db_path):
@@ -56,29 +57,135 @@ def drop_table(conn, table_name):
     execute_query(conn, drop_table)
 
 
-# def insert_data_to_table(conn, table, data):
-#     title = table[0]
-#     cols = table[1]
-#     cols_tuple = cols.split(", ")
-#     cols_tuple_len = len(cols_tuple)
-
-#     # create a string of '?' based on the amount of cols that the table has
-#     question_marks_str = ""
-#     for counter, _ in enumerate(cols_tuple):
-#         if cols_tuple_len == counter + 1:
-#             question_marks_str += "?"
-#             break
-
-#         question_marks_str += "?, "
-
-#     insert_with_param = f""" INSERT INTO {title}
-#                         ({cols})
-#                         VALUES ({question_marks_str}); """
-
-#     execute_query(conn, insert_with_param, data)
-
-
 def init_db(conn, table_name, file_path=None):
     df = pd.read_csv(file_path, na_values="---")
-
     df.to_sql(name=table_name, con=conn, if_exists="replace", index=False)
+
+    return df
+
+
+def create_dimension_table(fact_table, col_to_be_normalized, new_cols):
+    unique_values_lst = fact_table[col_to_be_normalized].unique()
+    ids = list(range(1, 1 + len(unique_values_lst)))
+
+    dim_table = pd.DataFrame(data=list(zip(ids, unique_values_lst)), columns=new_cols)
+
+    return dim_table
+
+
+def map_vals_to_ids(
+    fact_df, fact_col_name, dimension_df, dimension_keycol_name, dimension_refcol_name
+):
+    ### inspired by: https://stackoverflow.com/questions/53818434/pandas-replacing-values-by-looking-up-in-an-another-dataframe ###
+
+    # map each col value to an id
+    mapping_dict = dimension_df.set_index(dimension_refcol_name)[
+        dimension_keycol_name
+    ].to_dict()
+
+    # get all the values of the col from the fact table
+    col_vals = fact_df.filter(like=fact_col_name)
+
+    # replace all
+    fact_df[col_vals.columns] = col_vals.replace(mapping_dict)
+
+    return fact_df
+
+
+def normalize_data(conn, table1_df, table2_df):
+
+    ### create BookInventory fact table ###
+    bookInventory_df = table1_df[["Id", "Genre", "Title", "Author"]].copy()
+
+    # create and fill Genre table with unique values of Genre and add ids
+    genre_df = create_dimension_table(
+        bookInventory_df, "Genre", ["GenreKey", "GenreRef"]
+    )
+    bookInventory_df = map_vals_to_ids(
+        bookInventory_df, "Genre", genre_df, "GenreKey", "GenreRef"
+    )
+
+    # create and fill Title table with unique values of Genre and add ids
+    bookTitles_df = create_dimension_table(
+        bookInventory_df, "Title", ["BookTitleKey", "BookTitleRef"]
+    )
+    bookInventory_df = map_vals_to_ids(
+        bookInventory_df, "Title", bookTitles_df, "BookTitleKey", "BookTitleRef"
+    )
+
+    # create and fill Author table with unique values of Genre and add ids
+    bookAuthors_df = create_dimension_table(
+        bookInventory_df, "Author", ["BookAuthorKey", "BookAuthorRef"]
+    )
+    bookInventory_df = map_vals_to_ids(
+        bookInventory_df, "Author", bookAuthors_df, "BookAuthorKey", "BookAuthorRef"
+    )
+
+    # rename col names
+    bookInventory_df.rename(
+        columns={
+            "Id": "BookId",
+            "Genre": "GenreKey",
+            "Title": "BookTitleKey",
+            "Author": "BookAuthorKey",
+        },
+        inplace=True,
+    )
+
+    # add new column BookCopyKey to fact table and add the ids of book copies
+    bookInventory_df["BookCopyKey"] = list(range(1, 1 + len(bookInventory_df)))
+
+    # create new dimension table BookCopies
+    data = {
+        "BookCopyKey": bookInventory_df["BookCopyKey"],
+        "PurchasePrice£": table1_df["PurchasePrice£"],
+        "PurchaseDate": table1_df["PurchaseDate"],
+    }
+    bookCopies_df = pd.DataFrame(data)
+
+    # add all tables to db
+    bookInventory_df.to_sql(
+        name="BookInventory", con=conn, if_exists="replace", index=False
+    )
+    genre_df.to_sql(name="Genre", con=conn, if_exists="replace", index=False)
+    bookTitles_df.to_sql(name="BookTitle", con=conn, if_exists="replace", index=False)
+    bookAuthors_df.to_sql(name="BookAuthor", con=conn, if_exists="replace", index=False)
+    bookCopies_df.to_sql(name="BookCopies", con=conn, if_exists="replace", index=False)
+
+    ### create Transactions fact table ###
+    transactions_df = pd.DataFrame()
+    data = {
+        "TransactionId": table2_df["TransactionId"],
+        "BookId": table2_df["BookId"],
+        "BookCopyKey": table2_df["BookId"],
+        "TransactionType": np.nan,
+        "IsCheckedOut": np.nan,
+        "CheckedOutMemberId": np.nan,
+        "ReservedMemberId": np.nan,
+        "IsReserved": np.nan,
+        "TransactionTypeExpirationDate": np.nan,
+        "StartRecordDate": np.nan,
+        "EndRecordDate": np.nan,
+        "IsActive": np.nan,
+    }
+    transactions_df = pd.DataFrame(
+        data,
+        columns=[
+            "TransactionId",
+            "BookId",
+            "BookCopyKey",
+            "TransactionType",
+            "IsCheckedOut",
+            "CheckedOutMemberId",
+            "ReservedMemberId",
+            "IsReserved",
+            "TransactionTypeExpirationDate",
+            "StartRecordDate",
+            "EndRecordDate",
+            "IsActive",
+        ],
+    )
+
+    transactions_df.to_sql(
+        name="Transactions", con=conn, if_exists="replace", index=False
+    )
--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,5 @@
-import os
 from tkinter import Tk
+import os
 import database as d
 import gui
 
@@ -13,7 +13,6 @@ def main():
     conn = d.create_connection(db_path)
 
     if conn is not None:
-        # create initial tables
         create_book_info = [
             "Book_Info",
             "Id integer PRIMARY KEY",
@@ -34,66 +33,60 @@ def main():
             "FOREIGN KEY (BookId) REFERENCES Book_Info(BookId)",
         ]
 
+        # create two initial tables
         create_tables = [create_book_info, create_loan_reservation_history]
         for table in create_tables:
             d.create_table(conn, table)
 
-        # insert initial data to tables
-        fill_book_info = [
-            "Book_Info",
-            "Id, Genre, Title, Author, PurchasePrice£, PurchaseDate",
-        ]
-        fill_loan_reservation_history = [
-            "Loan_Reservation_History",
-            "TransactionId, BookId, ReservationDate, CheckoutDate, ReturnDate, MemberId",
-        ]
-
-        fill_tables = [fill_book_info, fill_loan_reservation_history]
-        for table in fill_tables:
-            table_name = table[0]
+        # fill initial tables with data from the two txt files
+        book_info_df = None
+        loan_res_hist_df = None
+        table_names = [create_book_info[0], create_loan_reservation_history[0]]
+        for table_name in table_names:
             file_path = f"{data_dir_path}{table_name}.txt"
-            d.init_db(conn, table_name, file_path)
 
-        # drop the initial tables
-        d.drop_table(conn, "Book_Info")
-        d.drop_table(conn, "Loan_Reservation_History")
+            # fill each initial table with data
+            df = d.init_db(conn, table_name, file_path)
+
+            # store the created df from the txt file to a variable
+            if table_name == create_book_info[0]:
+                book_info_df = df
+            else:
+                loan_res_hist_df = df
 
         # create tables for adding normalized data
         create_book_inventory = [
             "BookInventory",
             "BookId PRIMARY KEY",
-            "BookTitle TEXT NOT NULL",
-            "BookEdition INTEGER NOT NULL",
-            "BookAuthor TEXT NOT NULL",
             "GenreKey INTEGER",
+            "BookTitleKey INTEGER NOT NULL",
+            "BookAuthorKey TEXT NOT NULL",
             "BookCopyKey INTEGER",
             "FOREIGN KEY (GenreKey) REFERENCES Genre(GenreKey)",
+            "FOREIGN KEY (BookTitleKey) REFERENCES BookTitle(BookTitleKey)",
+            "FOREIGN KEY (BookAuthorKey) REFERENCES BookAuthor(BookAuthorKey)",
             "FOREIGN KEY (BookCopyKey) REFERENCES BookCopies(BookCopyKey)",
         ]
-        create_genre = ["Genre", "GenreKey PRIMARY KEY", "GenreRef TEXT NOT NULL"]
+        create_genre = [
+            "Genre",
+            "GenreKey PRIMARY KEY",
+            "GenreRef TEXT NOT NULL",
+        ]
+        create_bookTitle = [
+            "BookTitle",
+            "BookTitleKey PRIMARY KEY",
+            "BookTitleRef TEXT NOT NULL",
+        ]
+        create_bookAuthor = [
+            "BookAuthor",
+            "BookAuthorKey PRIMARY KEY",
+            "BookAuthorRef TEXT NOT NULL",
+        ]
         create_book_copies = [
             "BookCopies",
             "BookCopyKey PRIMARY KEY",
-            "PurchaseDate date",
-            "RetiredDate date",
-            "isActive INTEGER NOT NULL",
-        ]
-        create_member = [
-            "Member",
-            "MemberId PRIMARY KEY",
-            "Name TEXT NOT NULL",
-            "Surname TEXT NOT NULL",
-            "Address TEXT",
-            "Phone TEXT",
-            "StudentNumber TEXT",
-        ]
-        create_librarian = [
-            "Librarian",
-            "LibrarianId PRIMARY KEY",
-            "LibrarianName TEXT NOT NULL",
-            "LibrarianSurname TEXT NOT NULL",
-            "LibrarianAddress TEXT",
-            "LIbrarianPhone TEXT",
+            "PurchasePrice£ INTEGER NOT NULL",
+            "PurchaseDate DATE NOT NULL",
         ]
         create_transactions = [
             "Transactions",
@@ -103,7 +96,6 @@ def main():
             "TransactionType TEXT NOT NULL",
             "IsCheckedOut INTEGEGER NOT NULL",
             "CheckedOutMemberId INTEGER NOT NULL",
-            "LibrarianId INTEGER",
             "ReservedMemberId INTEGER",
             "IsReserved INTEGER",
             "TransactionTypeExpirationDate DATE NOT NULL",
@@ -112,20 +104,25 @@ def main():
             "IsActive INTEGER NOT NULL",
             "FOREIGN KEY (BookId) REFERENCES BookInventory(BookId)",
             "FOREIGN KEY (BookCopyKey) REFERENCES BookCopies(BookCopyKey)",
-            "FOREIGN KEY (LibrarianId) REFERENCES Librarian(LibrarianId)",
-            "FOREIGN KEY (ReservedMemberId) REFERENCES Member(MemberId)",
         ]
 
         create_tables = [
             create_book_inventory,
             create_genre,
+            create_bookTitle,
+            create_bookAuthor,
             create_book_copies,
-            create_member,
-            create_librarian,
             create_transactions,
         ]
         for table in create_tables:
             d.create_table(conn, table)
+
+        # normalize existing data
+        d.normalize_data(conn, book_info_df, loan_res_hist_df)
+
+        # drop the initial tables
+        d.drop_table(conn, "Book_Info")
+        d.drop_table(conn, "Loan_Reservation_History")
 
     else:
         print("Error! cannot create the database connection")
@@ -133,7 +130,7 @@ def main():
     conn.close()
 
     ## gui ###
-    # img_path = data_dir_path + 'gui_bg.png'
+    img_path = data_dir_path + "gui_bg.png"
 
     root = Tk()
     gui.app(root)
